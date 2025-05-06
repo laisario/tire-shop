981e827fc8c531b2ce38e2161dfc25339d44578b
Laisa Muito Linda <laisa.rioverde@gmail.com>
2025-05-06 13:29:22 -0300

    resolver problema swiper

--- src/app/(site)/(pages)/recapagem/page.tsx
+++ src/app/(site)/(pages)/recapagem/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Metadata } from "next";
 import Breadcrumb from '@/components/Common/Breadcrumb';
-// import Image from 'next/image';
+import Image from 'next/image';
 import SwiperCarrossel from '../../../../components/Swiper/index';
 
 export const metadata: Metadata = {
@@ -17,14 +17,14 @@ function RecapagemPage() {
       <section className="overflow-hidden pt-5 lg:pt-10 xl:pt-14 bg-gray-2">
         <div className="max-w-[750px] w-full mx-auto px-4 sm:px-8 xl:px-0">
           <div className="rounded-[10px] overflow-hidden mb-7.5">
-            {/* <Image
+            <Image
               className="rounded-[10px]"
               src="/images/recapagem/b269.png"
               alt="details"
               width={750}
               height={477}
-            /> */}
-            <SwiperCarrossel />
+            />
+            {/* <SwiperCarrossel /> */}
           </div>
 
           <div>
